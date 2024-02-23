73f607028f0831616e1a293cfe10848ba088944e
nikollllllas <nikollasohta2014@gmail.com>
2024-02-23 13:31:08 -0300

    feat: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { Header } from './components/header/Header'
+import { Header } from './components/header'
 import './global.css'
+import styles from './App.module.css'
+import { Post } from './components/post'
+import { Sidebar } from './components/sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
 
+      <div className={styles.wrapper}>
+        <Sidebar>sidebar</Sidebar>
+
+        <main>
+          <Post
+            author={'Nikollas'}
+            content={'This is the content of the post'}
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,31 @@
+import styles from './sidebar.module.css'
+import { PencilSimpleLine } from '@phosphor-icons/react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src='https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?q=-50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        alt=''
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src='https://github.com/nikollllllas.png'
+          alt=''
+        />
+        <strong>Nikollas Ohta</strong>
+        <span>web dev</span>
+      </div>
+
+      <footer>
+        <a href='#'>
+          <PencilSimpleLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
